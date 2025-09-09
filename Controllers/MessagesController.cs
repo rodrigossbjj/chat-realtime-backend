61749f91bd35c1e33a1fc642ca2983da54ed8827
rodrigossbjj <rodrigosousasales9@gmail.com>
2025-09-09 18:56:16 -0300

    feat(chat): adicionar envio de mensagens, histórico e proteção JWT no Hub e API

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -1,6 +1,30 @@
-﻿namespace chat_realtime_backend.Controllers
+﻿using chat_realtime_backend.Models;
+using chat_realtime_backend.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace chat_realtime_backend.Controllers
 {
-    public class MessagesController
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MessagesController : ControllerBase
     {
+        private readonly ChatService _chatService;
+
+        public MessagesController(ChatService chatService)
+        {
+            _chatService = chatService;
+        }
+
+        [HttpGet("{receiverId}")]
+        public async Task<IActionResult> GetMessages(int receiverId)
+        {
+            var senderId = int.Parse(User.FindFirst("id").Value);
+
+            var messages = await _chatService.GetMessages(senderId, receiverId);
+
+            return Ok(messages);
+        }
     }
 }
--- Services/ChatService.cs
+++ Services/ChatService.cs
@@ -1,5 +1,6 @@
 ﻿using chat_realtime_backend.Data;
 using chat_realtime_backend.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Threading.Tasks;
 
@@ -26,6 +27,16 @@ namespace chat_realtime_backend.Services
 
             _context.Messages.Add(message);
             await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Message>> GetMessages(int userId1, int userId2)
+        {
+            return await _context.Messages
+                .Where(m =>
+                    (m.SenderId == userId1 && m.ReceiverId == userId2) ||
+                    (m.SenderId == userId2 && m.ReceiverId == userId1))
+                .OrderBy(m => m.Timestamp)
+                .ToListAsync();
         }
     }
 }
